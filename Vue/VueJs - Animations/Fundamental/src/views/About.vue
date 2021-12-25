9ff464c38f6131e6caa0716b1e4657aaf905f3ac
Mysm <dev.mysm@gmail.com>
2021-12-25 18:51:40 +0330

    vueAnimation - fundamental - hooks

---
+++ Vue/VueJs - Animations/Fundamental/src/views/About.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="about">
+    <transition
+      appear
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+import gsap from "gsap";
+export default {
+  setup() {
+    const showTitle = ref(true);
+
+    const beforeEnter = (el) => {
+      gsap.to(el, {
+        y: -60,
+        opacity: 0,
+      });
+    };
+    const enter = (el) => {
+      gsap.to(el, {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        ease: "bounce.out",
+      });
+    };
+
+    const afterEnter = (el, done) => {
+      gsap.to(el, {
+        duration: 3,
+        y: 0,
+        opacity: 1,
+        ease: "bounce.out",
+        onComplete: done,
+      });
+    };
+
+    const beforeLeave = () => {};
+    const leave = () => {};
+    const afterLeave = () => {};
+
+    return {
+      showTitle,
+      beforeEnter,
+      enter,
+      afterEnter,
+      afterLeave,
+      leave,
+      beforeLeave,
+    };
+  },
+};
+</script>
+
+<style>
+.about {
+  max-width: 600px;
+  margin: 20px auto;
+}
+</style>
