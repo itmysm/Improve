8c93429e6d82c30d05190320411db44135461abd
Mysm <dev.mysm@gmail.com>
2021-12-31 19:02:41 +0330

    typescript - projects - understanding

---
+++ Typescript/Projects/understading-ts/scripts/02 - assignment.ts
@@ -0,0 +1,13 @@
+let num: number;
+num = 11;
+
+let num2: number;
+num2 = '2' // can not be a string or anything!!! just number
+
+
+
+
+// for auto assign
+
+let num3 = 11 // assign number to this var
+num = '2'
