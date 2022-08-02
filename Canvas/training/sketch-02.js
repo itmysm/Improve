787bcc0cffed1ca985e7fe91752ae632060a4950
Meysam Kiani <dev.mysm@gmail.com>
2022-08-02 21:51:14 +0430

    canvas - fundamental - sketch

---
+++ Canvas/training/sketch-02.js
@@ -0,0 +1,48 @@
+const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math')
+const random = require('canvas-sketch-util/random')
+
+const settings = {
+  dimensions: [1000, 1000]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+
+    const cx = width * 0.5
+    const cy = width * 0.5
+
+    const w = width * 0.01
+    const h = height * 0.1
+    let x, y
+
+    const num = 12;
+    const radius = width * 0.3
+
+    for (let i = 0; i < num; i++) {
+      const slice = math.degToRad(360 / num);
+      const angle = slice * i
+
+      x = radius * Math.sin(angle)
+      y = radius * Math.cos(angle)
+
+      context.save()
+      context.translate(cx, cy);
+      context.translate(x, y);
+      context.rotate(-angle)
+      context.scale(random.range(1, 3), 1)
+
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill()
+      context.restore()
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
+
