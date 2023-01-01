60039c605954969ab13f3c2a93866919336f5b7d
itmysm <itmysm@gmail.com>
2023-01-01 20:40:46 +0330

    refactor: create first app with react and work with props

---
+++ Front-End/React/01 - Basics/01-guide/src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import './ExpenseItem.css'
+
+function ExpenseItem (props) {
+    return (
+        <div className='expense-item'>
+            <div>
+                {props.expense.date.toISOString()}
+            </div>
+
+            <div className='expense-item__description'>
+                <h1>{props.expense.title}</h1>
+                <div className='expense-item__price'>{props.expense.amount}$</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
--- NodeJs/Fundamental/REPL/input.js
+++
@@ -1,8 +0,0 @@
-console.log('Hello World')
-
-// show inputs
-console.log(process.argv)
-
-
-const args = require('minimist')(process.argv.slice(2))
-console.log(args['name']);
