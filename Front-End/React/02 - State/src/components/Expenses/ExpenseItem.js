dfb69fd37c5536680fba38ee83f28dc68b6ea3d7
itmysm <itmysm@gmail.com>
2023-01-03 18:00:29 +0330

    work with state in React

--- Front-End/React/02 - State/src/components/Expenses/ExpenseItem.js
+++ Front-End/React/02 - State/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,22 @@
+import { useState } from 'react'
+
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.expense.title);
+
   const clickHandler = () => {
-    console.log('clicked!!!');
+    setTitle('Changed!!');
+    console.log(title);
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
 
       <div className='expense-item__description'>
-        <h1 className=''>{props.expense.title}</h1>
+        <h1 className=''>{title}</h1>
         <div className='expense-item__price'>{props.expense.amount}$</div>
       </div>
 
