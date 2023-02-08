04fa12a570ebba02204730caaed21a80cffd9cd8
itmysm <itmysm@gmail.com>
2023-02-08 15:36:06 +0330

    refactor: clean & remove useless files

--- Interview/chingal/src/main.js
+++ Interview/chingal/src/main.js
@@ -8,7 +8,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-import './assets/main.css'
+// import './assets/main.css'
 
 const app = createApp(App)
 const vuetify = createVuetify({
---
+++ Interview/chingal/src/stores/index.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const useCommends = defineStore('commends', {
+  state: () => {
+    return {
+
+    }
+  },
+
+  getters: {
+  },
+
+  actions: {
+  }
+})
