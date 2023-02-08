025b6a7fb7d81be49a5943c6c78cebac40d6cf98
itmysm <itmysm@gmail.com>
2023-02-08 15:30:05 +0330

    chore: add vuetify to project

--- Interview/chingal/src/main.js
+++ Interview/chingal/src/main.js
@@ -1,17 +1,23 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import vuetify from "./plugins/vuetify";
-
 
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import './assets/main.css'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 app.use(createPinia())
 app.use(router)
-app.use(vuetify);
+app.use(vuetify)
 
 app.mount('#app')
