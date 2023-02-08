efc1382108935faf5a33f2ff590ef2c7c96ec2d7
itmysm <itmysm@gmail.com>
2023-02-08 17:16:42 +0330

    init

--- Interview/chingal/src/main.js
+++ Interview/chingal/src/main.js
@@ -1,23 +1,6 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
+import './registerServiceWorker'
 import router from './router'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-// import './assets/main.css'
-
-const app = createApp(App)
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
-app.use(createPinia())
-app.use(router)
-app.use(vuetify)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
