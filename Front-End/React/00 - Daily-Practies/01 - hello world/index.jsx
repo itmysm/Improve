aa8ee08f1935a613e668b9aedce1c2f302f5b615
Meysam Kiani <dev.mysm@gmail.com>
2023-04-08 09:20:00 +0300

    that is the first react app in 2023

---
+++ Front-End/React/00 - Daily-Practies/01 - hello world/index.jsx
@@ -0,0 +1,18 @@
+function MyButton () {
+  return (
+    <button>
+      I'm a btn
+    </button>
+  )
+}
+
+export default function myApp () {
+  return (
+    <>
+      <h1>First App With React In 2023!</h1>
+      <br />
+      <br />
+      <MyButton />
+    </>
+  )
+}
